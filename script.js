2e1a308ceb66cc73cd0ee8ca4df7097971f44e3c
ayush <62694274+ayshptk@users.noreply.github.com>
2022-02-20 15:00:09 +0000

    add page rendering

---
+++ script.js
@@ -0,0 +1,26 @@
+const playarea = document.getElementById("page")
+
+
+document.onload = page("ayush")
+
+
+function page(pagename) {
+    playarea.innerHTML = "loading..."
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            playarea.innerHTML =
+                this.responseText;
+							document.title = pagename
+
+        } else if (this.readyState == 4 && this.status == 404) {
+            page(undefined)
+        }
+    };
+    xhttp.open("GET", "components/" + pagename + ".html", true);
+    xhttp.send();
+}
+
+function url(url) {
+    window.open(url, '_blank').focus();
+}
